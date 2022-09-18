9c78661fb4353d9422e0410203941e6ad97a1900
Sonal Shabir <103493764+sonal1502@users.noreply.github.com>
2022-09-18 22:53:37 +0530

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,52 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+# define MAX 5
+int A[MAX];
+int first=-1;
+
+void push(int data)
+{int i;
+first += 1; 
+for(i=first;i>0;i--)
+A[i]=A[i-1];
+A[0]=data;
+}
+
+int pop()
+{int i,value;
+value=A[0];
+for(i=0;i<first;i++)
+A[i]=A[i+1];
+first-=1;
+return value;
+}
+
+void Print()
+{int i;
+if(first==-1)
+{printf("STACK UNDERFLOW");
+exit(1);
+}
+for(i=0;i<=first;i++)
+printf("%d ",A[i]);
+printf("\n");
+}
+
+int main()
+{ int data;
+push(30);
+push(90);
+push(40);
+data=pop();
+Print();
+return 0;
+
+}
